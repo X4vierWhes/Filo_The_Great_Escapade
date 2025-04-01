b38bfd1d7e362072330e901d95466464d9101f4d
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2025-04-01 15:26:56 -0300

    wip: game_screen and player jump

--- scripts/characters/character_interface.gd
+++ scripts/characters/character_interface.gd
@@ -2,14 +2,13 @@ extends CharacterBody2D
 class_name CharacterInterface
 #Sinal de morte do personagem
 signal death
-#Estados em que um personagem pode se encontrar
-enum states {RUNNING, JUMP}
 
 #Parametros padrões para os personagens, usando export para serem editados no editor
 @export var speed: float = 0.0
 @export var hSpd: float = 0.0
 @export var vSpd: float = 0.0
 @export var gravity: float = 0.0
+var state: String = "idle"
 
 #Verificações padrões para os personagens
 var OnGround: bool = true
--- scripts/characters/obj_player.gd
+++ scripts/characters/obj_player.gd
@@ -1,27 +1,39 @@
 extends CharacterInterface
 class_name Player
 
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 @onready var anim_player: AnimatedSprite2D = $animPlayer
-var direction: Vector2 = Vector2.ZERO
+
+var current_state: String = "idle"
 
 func _ready() -> void:
-	anim_player.play("run")
+	state = "run"
+	current_state = state
+	anim_player.play(state)
 
 func _process(delta: float) -> void:
-	velocity = direction * speed
-	velocity.normalized()
+	print("Está no chão? ", is_on_ground())
+	if is_on_ground():
+		if velocity.y >= 0:
+			state = "run"
+			velocity.y = 0
+	else:
+		state = "jump"
+		velocity.y += gravity * delta
+	
+	anim_player.play(state)
+	print(velocity)
 	move_and_slide()
 
 func _on_input_component_input_detected(input_vector: Vector2) -> void:
 	if input_vector.y > 0: #Verifica se foi clicado o "move_down" e ignora se sim
 		return
 	
-	if input_vector.y == -1: #Se clicado o "move_up", chama a função de pulo do personagem
+	if input_vector.y == -1 && is_on_ground(): #Se clicado o "move_up", chama a função de pulo do personagem
 		jump()
 		return
-	
-	input_vector.y = 0 #existe um bug em que se apertar para cima e para algum lado o player sobe
-	direction = input_vector 
+		
+	velocity.x = input_vector.x * speed #direção horizontal
 
 
 func _on_input_component_action_detected(action: String) -> void:
@@ -29,7 +41,14 @@ func _on_input_component_action_detected(action: String) -> void:
 		attack()
 
 func jump() -> void:
-	print("pulei")
+	if is_on_ground():
+		print("Pulei")
+		state = "jump"
+		velocity.y = -vSpd
+	
 
 func attack() -> void:
-	print("ataquei")
+	state = "attacking"
+
+func is_on_ground() -> bool:
+	return ray_cast_2d.is_colliding()
