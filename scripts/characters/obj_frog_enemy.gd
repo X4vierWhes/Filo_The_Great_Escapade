3b8e05a5d0d679640c7dc1b89f2541b0d6e45884
João Lucas <joao.duarte31219@alunos.ufersa.edu.br>
2025-04-02 09:39:33 -0300

    feat: frog enemy object

---
+++ scripts/characters/obj_frog_enemy.gd
@@ -0,0 +1,41 @@
+extends Enemy
+
+@export var gravity:float = 25
+@export var jump_force:float = 500
+
+@export var wait_time_min:float = 0.5
+@export var wait_time_max:float = 3
+
+var vSpd:float = 50.0
+var on_ground:bool = true
+
+@onready var anim: AnimatedSprite2D = $anim
+
+@onready var timer: Timer = $Timer
+
+func _ready() -> void:
+	super._ready()
+	timer.start(randi_range(wait_time_min, wait_time_max))
+
+func _physics_process(delta: float) -> void:
+	if is_on_floor():
+		if velocity.y >= 0:
+			velocity.y = 0.0
+			if not anim.animation == "default":
+				swap_states()
+	else:
+		velocity.y += delta * gravity * gravity
+	
+	move_and_slide()
+
+
+func jump() -> void :
+	if is_on_floor():
+		print('vou pular')
+		velocity.y = -jump_force
+		swap_states()
+
+func swap_states() -> void:
+	$collision_jump.disabled = !$collision_jump.disabled
+	$collision.disabled = !$collision.disabled 
+	anim.play( "jump" if anim.animation == "default" else "default")
