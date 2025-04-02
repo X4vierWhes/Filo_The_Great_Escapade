97173cc68236a5112d7d717e998031ad0f76a3c3
João Lucas <joao.duarte31219@alunos.ufersa.edu.br>
2025-04-02 09:36:28 -0300

    feat: base enemy object

---
+++ scripts/characters/obj_enemy.gd
@@ -0,0 +1,14 @@
+class_name Enemy extends CharacterBody2D
+
+#@export var velocity:float = 585
+@export var speed:float = 100
+
+func _ready() -> void:
+	velocity = Vector2.LEFT * speed
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
