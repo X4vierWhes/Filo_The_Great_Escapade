1284511c78da393df496d91d4604870532a183ad
João Lucas <joao.duarte31219@alunos.ufersa.edu.br>
2025-04-03 19:29:54 -0300

    feat: add double jump component

---
+++ scenes/characters/jump_component.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var parent:CharacterBody2D = get_parent()
+
+func execute() -> void:
+	if parent.isAttacking or !parent.is_on_ground():
+		return
+	
+	parent.state = "jump"
+	parent.velocity.y = -parent.vSpd
