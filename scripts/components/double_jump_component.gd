3114c04c920ec744aa96683de5e58888ac89b005
João Lucas <joao.duarte31219@alunos.ufersa.edu.br>
2025-04-10 10:23:17 -0300

    fix: double jump gets one more jump than expected

--- scripts/components/double_jump_component.gd
+++ scripts/components/double_jump_component.gd
@@ -2,27 +2,32 @@ extends Node
 
 @onready var parent:CharacterBody2D = get_parent()
 
-var max_teste:int = 2
-var teste:int = 2
+var max_jump:int = 2
+var jump:int = 2
 
 func _physics_process(delta: float) -> void:
-	if teste < max_teste:
-		if parent.is_on_ground(): teste = max_teste
+	if jump < max_jump:
+		if parent.is_on_ground(): 
+			jump = max_jump
+			print('entrei')
 
 
 func execute() -> void:
+	print(jump)
 	if parent.isAttacking:
 		return
 	
-	if teste <= 0:
+	if jump <= 0:
 		return
 	
 	parent.state = "jump"
+	# extremamente horroroso mas se não tiver essa linha ele sempre vai dar um pulo a mais
+	parent.position.y -= 16 
 	parent.velocity.y = -parent.vSpd
 	
-	if teste == max_teste:
+	if jump == max_jump:
 		parent.audio.play_audio('jump')
 	else:
 		parent.audio.play_audio('doublejump')
 	
-	teste-=1
+	jump-=1
